8d9ebf7aaa336b859fa888b81fa8ac74496e6c52
schneidermichael <schneidermichael@outlook.com>
2022-04-22 21:43:48 +0200

    chore: added annotation

--- currency-service/src/main/java/at/ac/fhcampuswien/se/group1/currencyserviceapplication/repository/CurrenciesRepository.java
+++ currency-service/src/main/java/at/ac/fhcampuswien/se/group1/currencyserviceapplication/repository/CurrenciesRepository.java
@@ -2,6 +2,8 @@ package at.ac.fhcampuswien.se.group1.currencyserviceapplication.repository;
 
 import at.ac.fhcampuswien.se.group1.currencyserviceapplication.model.Currencies;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CurrenciesRepository extends MongoRepository<Currencies,String> {
 }
