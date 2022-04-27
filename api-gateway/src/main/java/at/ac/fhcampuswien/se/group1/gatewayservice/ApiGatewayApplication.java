97d023c28ad7fa4786de9113c40c9bc82dfbaeaf
schneidermichael <schneidermichael@outlook.com>
2022-04-27 22:57:29 +0200

    feat: added gateway configuration

--- api-gateway/src/main/java/at/ac/fhcampuswien/se/group1/gatewayservice/ApiGatewayApplication.java
+++ api-gateway/src/main/java/at/ac/fhcampuswien/se/group1/gatewayservice/ApiGatewayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class GatewayServiceApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
