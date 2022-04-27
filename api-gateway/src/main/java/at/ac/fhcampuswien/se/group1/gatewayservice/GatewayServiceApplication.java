9bab33d3d842c8c653fbb224aa03aa005856e0a3
schneidermichael <schneidermichael@outlook.com>
2022-04-27 22:52:00 +0200

    feat: added api-gateway

---
+++ api-gateway/src/main/java/at/ac/fhcampuswien/se/group1/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package at.ac.fhcampuswien.se.group1.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
