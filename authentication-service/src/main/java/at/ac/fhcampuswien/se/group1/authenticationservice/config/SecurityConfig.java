7becc53d0992af3d101fc78d1121a2ec7b1cd058
schneidermichael <schneidermichael@outlook.com>
2022-05-08 12:50:30 +0200

    feat: secure api gateway

--- authentication-service/src/main/java/at/ac/fhcampuswien/se/group1/authenticationservice/config/SecurityConfig.java
+++ authentication-service/src/main/java/at/ac/fhcampuswien/se/group1/authenticationservice/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
